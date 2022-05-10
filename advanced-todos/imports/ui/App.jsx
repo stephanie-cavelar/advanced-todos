ec2c757d071e7be749ae0d9ad73465e3e64ec96c
stephanie-cavelar <stephaniecostaavelar@gmail.com>
2022-05-10 14:55:34 -0300

    primeiro

--- advanced-todos/imports/ui/App.jsx
+++ advanced-todos/imports/ui/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
+import React from 'react';
+import { useTracker } from 'meteor/react-meteor-data';
+import { TasksCollection } from '/imports/api/TasksCollection';
+import { TaskForm } from './TaskForm';
 
-export const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello/>
-    <Info/>
-  </div>
-);
+export const App = () => {
+  const tasks = useTracker(() => TasksCollection.find({}).fetch());
+
+  return (
+    <div>
+      <h1>Welcome to Meteor!</h1>
+
+      <ul>
+        { tasks.map(task => <Task key={ task._id } task={ task }/>) }
+      </ul>
+      
+    </div>
+  );
+};
--- advanced-todos/server/main.js
+++ advanced-todos/server/main.js
@@ -6,26 +6,21 @@ function insertLink({ title, url }) {
 }
 
 Meteor.startup(() => {
-  // If the Links collection is empty, add some data.
-  if (LinksCollection.find().count() === 0) {
-    insertLink({
-      title: 'Do the Tutorial',
-      url: 'https://www.meteor.com/tutorials/react/creating-an-app'
-    });
-
-    insertLink({
-      title: 'Follow the Guide',
-      url: 'http://guide.meteor.com'
-    });
-
-    insertLink({
-      title: 'Read the Docs',
-      url: 'https://docs.meteor.com'
-    });
-
-    insertLink({
-      title: 'Discussions',
-      url: 'https://forums.meteor.com'
-    });
-  }
-});
+  import { Meteor } from 'meteor/meteor';
+  import { TasksCollection } from '/imports/api/TasksCollection';
+  
+  const insertTask = taskText => TasksCollection.insert({ text: taskText });
+  
+  Meteor.startup(() => {
+    if (TasksCollection.find().count() === 0) {
+      [
+        'First Task',
+        'Second Task',
+        'Third Task',
+        'Fourth Task',
+        'Fifth Task',
+        'Sixth Task',
+        'Seventh Task'
+      ].forEach(insertTask)
+    }
+  });
